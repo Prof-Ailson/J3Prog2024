506eca74ce076014186cbdcd1893e0d7a8667f71
Professor Aílson <102998807+Prof-Ailson@users.noreply.github.com>
2024-10-17 15:26:10 -0300

    R3

---
+++ J3TurmaCT-main/graficos/informacoesGlobais.js
@@ -0,0 +1,9 @@
+
+const url="https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/transporte/transporte-dados-globais.json"
+
+async function visualizarInformacoesGlobais(){
+    const resposta = await fetch (url);
+    let dados = await resposta.json;
+    console.log(dados);
+}
+visualizarInformacoesGlobais();
